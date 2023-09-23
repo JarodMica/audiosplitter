54a7f61f512dada8dadcee0e33e952c21fd22a56
Derek Tata <derekjtata@gmail.com>
2023-09-23 09:32:21 -0400

    Refactor and enhance `audiosplitter.py` for better readability and functionality
    ##### Changes Made
    
    1. **Imports Reorganized**: The import statements have been reorganized for better readability and to follow PEP 8 standards.
    2. **Function Names and Signatures**: Modified function names and their signatures to be more Pythonic and clear.
    3. **Type Annotations**: Added type annotations to function arguments and return types.
    4. **Removed Tkinter**: Removed the Tkinter GUI logic and switched to argparse for command-line arguments.
    5. **Code Style**: Applied PEP 8 formatting across the code.
    6. **Logic Refactor**: Replaced custom loops with Python built-in functions and idiomatic structures where applicable.
    7. **Comments and Docstrings**: Added and updated comments and docstrings for better code documentation.
    8. **Argument Parsing**: Added argparse for command-line argument parsing.
    
    ##### Revised Reasoning and Process
    
    1. **Imports and Code Style**: Reorganized imports and applied PEP 8 formatting to enhance code readability and maintenance.
    2. **Function Refactor**: Modified function names, signatures, and added type annotations for better readability and type checking.
    3. **UI Change**: Removed Tkinter to enable easier automation and system integration, replacing it with argparse for command-line interaction.
    4. **Logic Optimization**: Replaced custom loops with Pythonic structures for efficiency.
    5. **Documentation**: Updated comments and docstrings to clarify code functionality.
    
    ##### Pros and Cons of Changes
    
    - **Pros**:
      - Improved code readability and maintainability.
      - Easier to extend or integrate with other systems.
      - Type annotations provide better tooling and error checking.
    
    - **Cons**:
      - Removed GUI, which might affect users who prefer a graphical interface.
    
    ##### Testing
    
    The code was tested for various audio files and confirmed that the logic for audio splitting and segmenting remains functional.
    
    #### Summary
    
    This commit refactors the code for improved readability, maintainability, and functionality while also switching from a GUI to a command-line interface.

--- audiosplitter.py
+++ audiosplitter.py
@@ -1,74 +1,67 @@
-from scipy.io import wavfile
 import os
+import json
+import argparse
 import numpy as np
 from tqdm import tqdm
-import json
-from tkinter import filedialog
-from tkinter import *
+from pathlib import Path
+from scipy.io import wavfile
 from datetime import datetime, timedelta
 
-# Utility functions for AudioSeg
 
-def GetTime(video_seconds):
-    if (video_seconds < 0) :
-        return 00
+def get_time(video_seconds: float) -> str:
+    """Convert seconds to a time string format."""
+    if video_seconds < 0:
+        return "00"
+    sec = timedelta(seconds=video_seconds)
+    d = datetime(1, 1, 1) + sec
+    return f"{d.hour:02d}:{d.minute:02d}:{d.second:02d}.001"
 
-    else:
-        sec = timedelta(seconds=float(video_seconds))
-        d = datetime(1,1,1) + sec
 
-        instant = str(d.hour).zfill(2) + ':' + str(d.minute).zfill(2) + ':' + str(d.second).zfill(2) + str('.001')
-    
-        return instant
+def get_total_time(video_seconds: float) -> str:
+    """Get the total time as a string from seconds."""
+    sec = timedelta(seconds=video_seconds)
+    d = datetime(1, 1, 1) + sec
+    return f"{d.hour}:{d.minute}:{d.second}"
 
-def GetTotalTime(video_seconds):
-    sec = timedelta(seconds=float(video_seconds))
-    d = datetime(1,1,1) + sec
-    delta = str(d.hour) + ':' + str(d.minute) + ":" + str(d.second)
-    
-    return delta
 
-def windows(signal, window_size, step_size):
-    if type(window_size) is not int:
-        raise AttributeError("Window size must be an integer.")
-    if type(step_size) is not int:
-        raise AttributeError("Step size must be an integer.")
+def windows(signal: np.array, window_size: int, step_size: int):
+    """Generate windows of audio samples."""
     for i_start in range(0, len(signal), step_size):
         i_end = i_start + window_size
         if i_end >= len(signal):
             break
         yield signal[i_start:i_end]
 
-def energy(samples):
-    return np.sum(np.power(samples, 2.)) / float(len(samples))
 
-def rising_edges(binary_signal):
+def energy(samples: np.array) -> float:
+    """Calculate the energy of a sample window."""
+    return np.sum(np.power(samples, 2.0)) / float(len(samples))
+
+
+def rising_edges(binary_signal: np.array):
+    """Generate rising edges in a binary signal."""
     previous_value = 0
-    index = 0
-    for x in binary_signal:
+    for index, x in enumerate(binary_signal):
         if x and not previous_value:
             yield index
         previous_value = x
-        index += 1
 
-def run_audioseg():
-    '''
+
+def run_audioseg(input_file: Path, output_dir: Path) -> None:
+    """
     Last Acceptable Values
 
     min_silence_length = 0.3
     silence_threshold = 1e-3
     step_duration = 0.03/10
 
-    '''
-    root = root_audioseg
-    root.withdraw()
-    input_file = filedialog.askopenfilename(parent=root_audioseg)
+    """
     if not input_file:
         raise ValueError("No file selected.")
-    output_dir = input("Please enter the name of the output folder: ")
+
     min_silence_length = 0.6
     silence_threshold = 1e-4
-    step_duration = 0.03/10
+    step_duration = 0.03 / 10
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -76,23 +69,22 @@ def run_audioseg():
     input_filename = input_file
     window_duration = min_silence_length
     if step_duration is None:
-        step_duration = window_duration / 10.
+        step_duration = window_duration / 10.0
     else:
         step_duration = step_duration
 
     output_filename_prefix = os.path.splitext(os.path.basename(input_filename))[0]
     dry_run = False
 
-    print("Splitting {} where energy is below {}% for longer than {}s.".format(
-        input_filename,
-        silence_threshold * 100.,
-        window_duration
+    print(
+        "Splitting {} where energy is below {}% for longer than {}s.".format(
+            input_filename, silence_threshold * 100.0, window_duration
         )
     )
 
     # Read and split the file
 
-    sample_rate, samples = input_data=wavfile.read(filename=input_filename, mmap=True)
+    sample_rate, samples = input_data = wavfile.read(filename=input_filename, mmap=True)
 
     max_amplitude = np.iinfo(samples.dtype).max
     print(max_amplitude)
@@ -104,15 +96,13 @@ def run_audioseg():
     step_size = int(step_duration * sample_rate)
 
     signal_windows = windows(
-        signal=samples,
-        window_size=window_size,
-        step_size=step_size
+        signal=samples, window_size=window_size, step_size=step_size
     )
 
-    window_energy = (energy(w) / max_energy for w in tqdm(
-        signal_windows,
-        total=int(len(samples) / float(step_size))
-    ))
+    window_energy = (
+        energy(w) / max_energy
+        for w in tqdm(signal_windows, total=int(len(samples) / float(step_size)))
+    )
 
     window_silence = (e > silence_threshold for e in window_energy)
 
@@ -123,65 +113,63 @@ def run_audioseg():
     cut_samples = [int(t * sample_rate) for t in cut_times]
     cut_samples.append(-1)
 
-    cut_ranges = [(i, cut_samples[i], cut_samples[i+1]) for i in range(len(cut_samples) - 1)]
+    cut_ranges = [
+        (i, cut_samples[i], cut_samples[i + 1]) for i in range(len(cut_samples) - 1)
+    ]
 
-    video_sub = {str(i) : [str(GetTime(((cut_samples[i])/sample_rate))), 
-                        str(GetTime(((cut_samples[i+1])/sample_rate)))] 
-                for i in range(len(cut_samples) - 1)}
+    video_sub = {
+        str(i): [
+            str(get_time(((cut_samples[i]) / sample_rate))),
+            str(get_time(((cut_samples[i + 1]) / sample_rate))),
+        ]
+        for i in range(len(cut_samples) - 1)
+    }
 
     for i, start, stop in tqdm(cut_ranges):
         output_file_path = "{}_{:03d}.wav".format(
-            os.path.join(output_dir, output_filename_prefix),
-            i
+            os.path.join(output_dir, output_filename_prefix), i
         )
         if not dry_run:
             print("Writing file {}".format(output_file_path))
             wavfile.write(
-                filename=output_file_path,
-                rate=sample_rate,
-                data=samples[start:stop]
+                filename=output_file_path, rate=sample_rate, data=samples[start:stop]
             )
         else:
             print("Not writing file {}".format(output_file_path))
-            
-    with open (output_dir+'\\'+output_filename_prefix+'.json', 'w') as output:
+
+    with open(output_dir / f"{output_filename_prefix}.json", "w") as output:
         json.dump(video_sub, output)
 
 
-def run_audiosplitter():
-    # Create the Tkinter root window
-    root = root_audiosplitter
-    root.withdraw()
+def run_audiosplitter(input_directory: Path):
+    """Run the audio splitting logic."""
+    if not input_directory.exists():
+        print("No such directory.")
+        return
 
-    # Ask the user to select the audio file directory using the file explorer
-    input_directory = filedialog.askdirectory(title="Select Audio File Directory", parent=root_audiosplitter)
+    for audio_file in input_directory.glob("*.wav"):
+        split_audio_file(audio_file)
 
-    # Check if a directory was selected
-    if input_directory:
-        # Iterate over the files in the directory
-        for filename in os.listdir(input_directory):
-            if filename.endswith(".wav"):
-                file_path = os.path.join(input_directory, filename)
-                split_audio_file(file_path)
-    else:
-        print("No directory selected.")
 
-    # Close the Tkinter root window
-    root.destroy()
+def split_audio_file(file_path: Path, segment_duration: int = 10) -> None:
+    """Split an audio file into segments of a given duration.
 
-def split_audio_file(file_path, segment_duration=10):
+    Parameters:
+        file_path: Path to the audio file to split.
+        segment_duration: Duration of each segment in seconds.
+    """
     # Load the audio file
-    sample_rate, audio_data = wavfile.read(file_path)
+    sample_rate, audio_data = wavfile.read(str(file_path))
 
     # Calculate the number of segments
-    num_segments = int(len(audio_data) / (sample_rate * segment_duration))
+    num_segments = len(audio_data) // (sample_rate * segment_duration)
     remainder = len(audio_data) % (sample_rate * segment_duration)
     if remainder > 0:
         num_segments += 1
 
     # Create the output directory for segments
-    output_dir = os.path.dirname(file_path)
-    base_filename = os.path.splitext(os.path.basename(file_path))[0]
+    output_dir = file_path.parent
+    base_filename = file_path.stem
 
     # Split the audio file into segments
     for i in range(num_segments):
@@ -190,32 +178,40 @@ def split_audio_file(file_path, segment_duration=10):
         segment = audio_data[start:end]
 
         # Create the output file name
-        segment_filename = f"{base_filename}_{i+1}.wav"
-        segment_path = os.path.join(output_dir, segment_filename)
+        segment_filename = f"{base_filename}_{i + 1}.wav"
+        segment_path = output_dir / segment_filename
 
         # Save the segment as a new WAV file
-        wavfile.write(segment_path, sample_rate, segment)
+        wavfile.write(str(segment_path), sample_rate, segment)
 
-        print(f"Segment {i+1}/{num_segments} saved: {segment_filename}")
+        print(f"Segment {i + 1}/{num_segments} saved: {segment_filename}")
 
-    os.remove(file_path)
+    file_path.unlink()
 
-# Create the Tkinter root windows
-root_audioseg = Tk()
-root_audioseg.withdraw()
-root_audiosplitter = Tk()
-root_audiosplitter.withdraw()
 
-# Ask the user to select which script to run
-user_choice = input("Do you want to use:\n1) Audiosegmenter (removes silence)\n2) Audiosplitter (cuts into 10 second files)")
+def main():
+    parser = argparse.ArgumentParser(description="Audio Processing Script")
+    parser.add_argument("--mode", choices=["audioseg", "audiosplitter"], required=True)
+    parser.add_argument("--input_file", type=Path, help="Audio file to split.")
+    parser.add_argument("--output_dir", type=Path, help="Output directory.")
+    parser.add_argument("--input_directory", type=Path, help="Input directory.")
 
-if user_choice == '1':
-    run_audioseg()
-elif user_choice == '2':
-    run_audiosplitter()
-else:
-    print("Invalid choice. Please enter either 1 or 2.")
+    args = parser.parse_args()
 
-# Close the Tkinter root windows
-root_audioseg.destroy()
-root_audiosplitter.destroy()
+    match args.mode:
+        case "audioseg":
+            if not args.input_file or not args.output_dir:
+                print(
+                    "Both --input_file and --output_dir must be specified for audioseg."
+                )
+            else:
+                run_audioseg(args.input_file, args.output_dir)
+        case "audiosplitter":
+            if not args.input_directory:
+                print("--input_directory must be specified for audiosplitter.")
+            else:
+                run_audiosplitter(args.input_directory)
+
+
+if __name__ == "__main__":
+    main()
